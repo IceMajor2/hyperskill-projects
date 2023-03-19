1a18e3ca5ff01570b065e521a38868cfe6bb46e8
IceMajor2 <icemajorr2@gmail.com>
2023-03-19 13:25:12 +0100

    fix - substring

--- ChuckNorrisCipherEncoder/src/main/java/ChuckNorrisCipherEncoder.java
+++ ChuckNorrisCipherEncoder/src/main/java/ChuckNorrisCipherEncoder.java
@@ -4,6 +4,8 @@ public class ChuckNorrisCipherEncoder {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Input string:");
         String input = scanner.nextLine();
         for(char ch : input.toCharArray()) {
             System.out.print(ch + " ");
