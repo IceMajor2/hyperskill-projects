ade8fa7faca225015d74abedde066257c1db288d
IceMajor <icemajorr2@gmail.com>
2023-04-09 13:48:01 +0200

    H2 database implemented

--- CarSharing/src/main/java/carsharing/CarSharing.java
+++ CarSharing/src/main/java/carsharing/CarSharing.java
@@ -1,8 +1,40 @@
 package carsharing;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class CarSharing {
 
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "org.h2.Driver";
+    static final String DB_URL = "jdbc:h2:~/test";
+
+    // Database credentials
+    static final String USER = "sa";
+    static final String PASS = "";
+
     public static void main(String[] args) {
-        
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            // Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            // Open a connection
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            // Execute a query
+                stmt = conn.createStatement();
+            
+            // Clean-up environment
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
