634dce4a8e5a348b25e4af5b0e0f55cd1fa18559
IceMajor <icemajorr2@gmail.com>
2023-05-31 06:42:01 +0200

    Payment class define

--- Account Service/Account Service/task/src/account/models/Payment.java
+++ Account Service/Account Service/task/src/account/models/Payment.java
@@ -1,4 +1,53 @@
 package account.models;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "payments")
 public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @Column
+    private LocalDate date;
+    @Column
+    private Long salary;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Long salary) {
+        this.salary = salary;
+    }
 }
