1032b3b129ee37ee60f821ea95af7e97f395e7ee
IceMajor <icemajorr2@gmail.com>
2023-05-31 10:27:56 +0200

    POST request for '/api/acct/payments' does not throw 500 anymore (BAD_REQUEST now)

--- Account Service/Account Service/task/src/account/exceptions/CustomRestExceptionHandler.java
+++ Account Service/Account Service/task/src/account/exceptions/CustomRestExceptionHandler.java
@@ -1,5 +1,8 @@
 package account.exceptions;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Path;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
@@ -8,6 +11,7 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
@@ -35,7 +39,28 @@ public class CustomRestExceptionHandler extends ResponseEntityExceptionHandler {
         String path = ((ServletWebRequest) request).getRequest().getRequestURI();
         ApiError apiError =
                 new ApiError(HttpStatus.BAD_REQUEST, errors.get(0), path);
-        return handleExceptionInternal(
-                ex, apiError, headers, ex.getStatusCode(), request);
+        return handleExceptionInternal
+                (ex, apiError, headers, ex.getStatusCode(), request);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ApiError> handleConstraintViolationException(ConstraintViolationException exception) {
+        String message = "Body is invalid";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        String path = getPath(exception);
+
+        ApiError error = new ApiError(status, message, path);
+        return ResponseEntity.badRequest().body(error);
+    }
+
+    private String getPath(ConstraintViolationException exception) {
+        String path = null;
+        ConstraintViolation<?> violation = exception.getConstraintViolations()
+                .iterator().next();
+        // last node is path
+        for(Path.Node node : violation.getPropertyPath()) {
+            path = node.getName();
+        }
+        return path;
     }
 }
