42a5bca25fed70e1aae9f736d7f4fbc3da743b9e
IceMajor <icemajorr2@gmail.com>
2023-06-01 17:50:31 +0200

    Failed logins (bad credentials) are also logged

--- Account Service/Account Service/task/src/account/security/AuthenticationFailureListener.java
+++ Account Service/Account Service/task/src/account/security/AuthenticationFailureListener.java
@@ -1,27 +1,27 @@
-//package account.security;
-//
-//import account.services.LoginAttemptService;
-//import jakarta.servlet.http.HttpServletRequest;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.ApplicationListener;
-//import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class AuthenticationFailureListener
-//        implements ApplicationListener<AuthenticationFailureBadCredentialsEvent> {
-//
-//    @Autowired
-//    private HttpServletRequest request;
-//
-//    @Autowired
-//    private LoginAttemptService loginAttemptService;
-//
-//    @Override
-//    public void onApplicationEvent(AuthenticationFailureBadCredentialsEvent event) {
-//        final String username = event.getAuthentication().getName();
-//        if (username != null) {
-//            loginAttemptService.loginFailed(username, request.getRequestURI());
-//        }
-//    }
-//}
+package account.security;
+
+import account.services.LoginAttemptService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationFailureListener
+        implements ApplicationListener<AuthenticationFailureBadCredentialsEvent> {
+
+    @Autowired
+    private HttpServletRequest request;
+
+    @Autowired
+    private LoginAttemptService loginAttemptService;
+
+    @Override
+    public void onApplicationEvent(AuthenticationFailureBadCredentialsEvent event) {
+        final String username = event.getAuthentication().getName();
+        if (username != null) {
+            loginAttemptService.loginFailed(username, request.getRequestURI());
+        }
+    }
+}
--- Account Service/Account Service/task/src/account/services/LoginAttemptService.java
+++ Account Service/Account Service/task/src/account/services/LoginAttemptService.java
@@ -1,22 +1,22 @@
-//package account.services;
-//
-//import account.enums.SecurityAction;
-//import account.models.SecurityLog;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class LoginAttemptService {
-//
-//    @Autowired
-//    private SecurityLogService securityLogService;
-//
-//    public void loginFailed(final String userEmail, String URI) {
-//        var action = SecurityAction.ACCESS_DENIED;
-//        String subject = userEmail;
-//        String object = URI;
-//        String path = URI;
-//        SecurityLog log = new SecurityLog(action, subject, object, path);
-//        securityLogService.saveLog(log);
-//    }
-//}
+package account.services;
+
+import account.enums.SecurityAction;
+import account.models.SecurityLog;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginAttemptService {
+
+    @Autowired
+    private SecurityLogService securityLogService;
+
+    public void loginFailed(final String userEmail, String URI) {
+        var action = SecurityAction.LOGIN_FAILED;
+        String subject = userEmail;
+        String object = URI;
+        String path = URI;
+        SecurityLog log = new SecurityLog(action, subject, object, path);
+        securityLogService.saveLog(log);
+    }
+}
