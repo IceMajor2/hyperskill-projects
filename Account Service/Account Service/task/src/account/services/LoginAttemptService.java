ce65ecfe99fc40f49dd86c37538c1801f288ba3f
IceMajor <icemajorr2@gmail.com>
2023-06-01 23:18:49 +0200

    Unsuccessful authentications are counted for each user

--- Account Service/Account Service/task/src/account/services/LoginAttemptService.java
+++ Account Service/Account Service/task/src/account/services/LoginAttemptService.java
@@ -2,16 +2,38 @@ package account.services;
 
 import account.enums.SecurityAction;
 import account.models.SecurityLog;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class LoginAttemptService {
 
+    private static final int MAX_ATTEMPTS = 5;
+    private Map<String, Integer> attempts;
     @Autowired
     private SecurityLogService securityLogService;
 
+    public LoginAttemptService() {
+        super();
+        attempts = new HashMap<>();
+    }
+
     public void loginFailed(final String userEmail, String URI) {
+        saveLogWhenLoginFailed(userEmail, URI);
+        int usrAttempts = attempts.getOrDefault(userEmail, 0);
+        usrAttempts++;
+        attempts.put(userEmail, usrAttempts);
+    }
+
+    public boolean isBlocked(final String userEmail) {
+        return attempts.getOrDefault(userEmail, 0) > MAX_ATTEMPTS;
+    }
+
+    private void saveLogWhenLoginFailed(final String userEmail, String URI) {
         var action = SecurityAction.LOGIN_FAILED;
         String subject = userEmail;
         String object = URI;
