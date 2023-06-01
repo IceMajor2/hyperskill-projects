91f30748518f1f5d7a8ce1e495748f662a1705aa
IceMajor <icemajorr2@gmail.com>
2023-06-01 23:25:42 +0200

    Move UserDetailsService to 'services' package

--- Account Service/Account Service/task/src/account/services/UserDetailsServiceImpl.java
+++ Account Service/Account Service/task/src/account/services/UserDetailsServiceImpl.java
@@ -1,6 +1,7 @@
-package account.security;
+package account.services;
 
 import account.repositories.UserRepository;
+import account.security.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
