74953086fa7184976f9f01f5a5567420f075059b
IceMajor <icemajorr2@gmail.com>
2023-06-03 09:04:51 +0200

    Start Stage 2/5

--- Music Advisor/Music Advisor/task/test/MusicAdvisorTest.java
+++ Music Advisor/Music Advisor/task/test/MusicAdvisorTest.java
@@ -18,59 +18,61 @@ public class MusicAdvisorTest extends StageTest<Attach> {
     @Override
     public List<TestCase<Attach>> generate() {
         return List.of(
-                new TestCase<Attach>()
-                        .setInput("featured\nnew\nexit")
-                        .setAttach(new Attach(reply -> {
-                            if (reply.split("\n").length == 0 || !reply.split("\n")[0].contains("---FEATURED---")) {
-                                return new CheckResult(false,
-                                        "Wrong answer!");
-                            }
-                            if (!reply.contains("---NEW RELEASES---")) {
-                                return new CheckResult(false,
-                                        "Wrong answer!");
-                            }
-                            return CheckResult.correct();
-                        })),
-                new TestCase<Attach>()
-                        .setInput("new\nexit")
-                        .setAttach(new Attach(reply -> {
-                            if (reply.split("\n").length == 0 || !reply.split("\n")[0].contains("---NEW RELEASES---")) {
-                                return new CheckResult(false,
-                                        "Wrong answer!");
-                            }
-                            return CheckResult.correct();
-                        })),
-                new TestCase<Attach>()
-                        .setInput("categories\nexit")
-                        .setAttach(new Attach(reply -> {
-                            if (reply.split("\n").length == 0 || !reply.split("\n")[0].contains("---CATEGORIES---")) {
-                                return new CheckResult(false,
-                                        "Wrong answer!");
-                            }
-                            return CheckResult.correct();
-                        })),
-                new TestCase<Attach>()
-                        .setInput("playlists Mood\ncategories\nexit")
-                        .setAttach(new Attach(reply -> {
-                            if (reply.split("\n").length == 0 || !reply.split("\n")[0].contains("PLAYLISTS---")) {
-                                return new CheckResult(false,
-                                        "Wrong answer!");
-                            }
-                            if (!reply.contains("---CATEGORIES---")) {
-                                return new CheckResult(false,
-                                        "Wrong answer!");
-                            }
-                            return CheckResult.correct();
-                        })),
-                new TestCase<Attach>()
-                        .setInput("exit\n")
-                        .setAttach(new Attach(reply -> {
-                            if (reply.split("\n").length == 0 || !reply.split("\n")[0].contains("---GOODBYE!---")) {
-                                return new CheckResult(false,
-                                        "Wrong answer!");
-                            }
-                            return CheckResult.correct();
-                        }))
+            new TestCase<Attach>()
+                .setInput("auth\nexit")
+                .setAttach(new Attach(reply -> {
+                    if (!reply.contains("---SUCCESS---")) {
+                        return new CheckResult(false,
+                            "There is no \"---SUCCESS---\" after \"auth\" but should be");
+                    }
+                    if (!reply.contains("https://accounts.spotify.com/authorize?")) {
+                        return new CheckResult(false,
+                            "There is no link after \"auth\" but should be");
+                    }
+//                    if (reply.contains("a19ee7dbfda443b2a8150c9101bfd645")) {
+//                        return new CheckResult(false,
+//                                "You shouldn't use the client_id from the example!! " +
+//                                        "You should create your own id on the spotify site.");
+//                    }
+                    return CheckResult.correct();
+                })),
+
+            new TestCase<Attach>()
+                .setInput("new\nexit")
+                .setAttach(new Attach(reply -> {
+                    if (!reply.strip().startsWith("Please, provide access for application.")) {
+                        return new CheckResult(false,
+                            "When no access provided you should output " +
+                                "\"Please, provide access for application.\"");
+                    }
+                    return CheckResult.correct();
+                })),
+
+            new TestCase<Attach>()
+                .setInput("featured\nexit")
+                .setAttach(new Attach(reply -> {
+                    if (!reply.strip().startsWith("Please, provide access for application.")) {
+                        return new CheckResult(false,
+                            "When no access provided you should output " +
+                                "\"Please, provide access for application.\"");
+                    }
+                    return CheckResult.correct();
+                })),
+
+            new TestCase<Attach>()
+                .setInput("auth\nnew\nfeatured\nexit")
+                .setAttach(new Attach(reply -> {
+                    if (!reply.contains("---NEW RELEASES---")) {
+                        return new CheckResult(false,
+                            "When \"new\" was inputted there should be \"---NEW RELEASES---\" line");
+                    }
+                    if (!reply.contains("---FEATURED---")) {
+                        return new CheckResult(false,
+                            "When \"featured\" was inputted there should be \"---FEATURED---\" line");
+                    }
+                    return CheckResult.correct();
+                }))
+
         );
     }
 
