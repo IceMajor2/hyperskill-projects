ee74373a34fcbf7c5577eda864f1aef17027cc3d
IceMajor <icemajorr2@gmail.com>
2023-06-04 20:31:30 +0200

    Create Category model

---
+++ Music Advisor/Music Advisor/task/src/advisor/models/Category.java
@@ -0,0 +1,17 @@
+package advisor.models;
+
+public class Category {
+
+    private int id;
+    private String name;
+
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
