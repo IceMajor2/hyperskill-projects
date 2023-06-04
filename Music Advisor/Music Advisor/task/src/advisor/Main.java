a585b061704a6cc21fe2141c39a41151ef7457f6
IceMajor <icemajorr2@gmail.com>
2023-06-04 18:37:37 +0200

    Add setArgs(args) method execution at beginning to specify constants

--- Music Advisor/Music Advisor/task/src/advisor/Main.java
+++ Music Advisor/Music Advisor/task/src/advisor/Main.java
@@ -6,13 +6,7 @@ public class Main {
     public static String RESOURCE_ARGUMENT;
 
     public static void main(String[] args) throws Exception {
-
-        if(args.length == 0) {
-            ACCESS_ARGUMENT = "https://accounts.spotify.com";
-        } else {
-            ACCESS_ARGUMENT = args[1];
-        }
-
+        setArgs(args);
         UserInterface userInterface = new UserInterface();
         userInterface.run();
     }
