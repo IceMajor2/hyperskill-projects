55827dea95e72454edda03a74a792566faacb149
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-06-09 13:48:41 +0200

    Write unit test checking title of page on '/code'

--- code-sharing-platform/src/test/java/platform/CodeSharingPlatformApplicationTests.java
+++ code-sharing-platform/src/test/java/platform/CodeSharingPlatformApplicationTests.java
@@ -1,19 +1,33 @@
 package platform;
 
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 
 @SpringBootTest
 class CodeSharingPlatformApplicationTests {
 
-    @Autowired
-    TestRestTemplate restTemplate;
+    private static WebClient webClient;
 
-    @Test
-    public void getCodeContainsTagPreAndTitleCode() {
-
+    @BeforeAll
+    public static void init() {
+        webClient = new WebClient();
     }
 
+    @AfterAll
+    public static void close() {
+        webClient.close();
+    }
+
+    @Test
+    public void ensureRightTitleTest() throws Exception {
+        webClient.getOptions().setThrowExceptionOnScriptError(false);
+        HtmlPage page = webClient.getPage("/code");
+
+        Assertions.assertEquals("Code", page.getTitleText());
+    }
 }
