a1db533102ec4dd099dbdfb7ad5617f937d398f5
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-06-09 15:42:30 +0200

    Write simple test for driving '/code' endpoint development

--- code-sharing-platform/src/test/java/platform/CodeSharingPlatformApplicationTests.java
+++ code-sharing-platform/src/test/java/platform/CodeSharingPlatformApplicationTests.java
@@ -2,11 +2,14 @@ package platform;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CodeSharingPlatformApplicationTests {
@@ -15,10 +18,27 @@ class CodeSharingPlatformApplicationTests {
     TestRestTemplate restTemplate;
 
     @Test
+    public void shouldReturnCorrectHeaders() {
+        ResponseEntity response = restTemplate.getForEntity("/", String.class);
+
+        Assert.assertEquals(MediaType.TEXT_HTML,
+                response.getHeaders().getContentType());
+    }
+
+    @Test
     public void titleShouldBeCodeTest() {
-        String responseBody = restTemplate.getForObject("/", String.class);
-        Document doc = Jsoup.parse(responseBody);
+        String response = restTemplate.getForObject("/", String.class);
+        Document doc = Jsoup.parse(response);
 
         Assert.assertEquals("Code", doc.title());
+    }
+
+    @Test
+    public void preTagShouldNotBeEmpty() {
+        String response = restTemplate.getForObject("/", String.class);
+        Document doc = Jsoup.parse(response);
+
+        Elements elements = doc.select("pre");
+        Assert.assertTrue(elements.hasText());
     }
 }
