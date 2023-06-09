6ee4942e230a1f7d8cb25aaad9abb7231f8fcf3b
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-06-09 16:31:25 +0200

    Create api endpoint

--- code-sharing-platform/src/main/java/platform/CodeSharingPlatformController.java
+++ code-sharing-platform/src/main/java/platform/CodeSharingPlatformController.java
@@ -1,14 +1,25 @@
 package platform;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
 
 @Controller
 public class CodeSharingPlatformController {
 
-    @GetMapping("/code")
+    @GetMapping(value = {"/code", "/codes"})
     public String getCode() {
         // no need to explicitly define headers. Spring, I guess, does that for me
         return "index";
+    }
+
+    @GetMapping(value = {"/api/code", "/api/code/"})
+    public ResponseEntity getApiCode() {
+        String code = "public static void main(String[] args) {\n" +
+                "    SpringApplication.run(CodeSharingPlatform.class, args);\n" +
+                "}";
+        return ResponseEntity.ok(Map.of("code", code));
     }
 }
