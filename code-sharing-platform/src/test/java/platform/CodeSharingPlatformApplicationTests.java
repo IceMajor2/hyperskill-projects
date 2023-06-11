f4484088462e75182826b0aa9fd5706b821280ac
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-06-11 18:44:33 +0200

    Change annotation to execute function before each test

--- code-sharing-platform/src/test/java/platform/CodeSharingPlatformApplicationTests.java
+++ code-sharing-platform/src/test/java/platform/CodeSharingPlatformApplicationTests.java
@@ -6,7 +6,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.junit.Assert;
-import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,7 +20,6 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CodeSharingPlatformApplicationTests {
@@ -29,7 +28,7 @@ class CodeSharingPlatformApplicationTests {
     TestRestTemplate restTemplate;
     private String setupTime;
 
-    @Before
+    @BeforeEach
     public void setup() {
         String jsonRequest = "{ \"code\": \"int a = 0;\" }";
         HttpEntity<String> request = new HttpEntity<>(jsonRequest);
@@ -71,7 +70,7 @@ class CodeSharingPlatformApplicationTests {
         DocumentContext documentContext = JsonPath.parse(response.getBody());
         String code = documentContext.read("$.code");
         String date = documentContext.read("$.date");
-
+        
         assertEquals(code, "int a = 0;");
         assertEquals(date, this.setupTime);
     }
