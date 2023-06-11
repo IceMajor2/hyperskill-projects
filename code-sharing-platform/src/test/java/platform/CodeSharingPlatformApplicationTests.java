77566999f2843941119bbac9b518f3ecc5e6b6ac
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-06-11 19:11:08 +0200

    Write test checking equality between web and api code + more elastic date asserting

--- code-sharing-platform/src/test/java/platform/CodeSharingPlatformApplicationTests.java
+++ code-sharing-platform/src/test/java/platform/CodeSharingPlatformApplicationTests.java
@@ -4,6 +4,7 @@ import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,9 +18,11 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CodeSharingPlatformApplicationTests {
@@ -70,7 +73,9 @@ class CodeSharingPlatformApplicationTests {
         DocumentContext documentContext = JsonPath.parse(response.getBody());
         String code = documentContext.read("$.code");
         String date = documentContext.read("$.date");
-        
+
+        assertTrue(dateCheck(date, this.setupTime));
+
         assertEquals(code, "int a = 0;");
         assertEquals(date, this.setupTime);
     }
@@ -83,5 +88,37 @@ class CodeSharingPlatformApplicationTests {
                 response.getHeaders().getContentType().getSubtype());
 
         Assert.assertEquals(MediaType.APPLICATION_JSON.toString(), contentTypeStr);
+    }
+
+    @Test
+    public void apiCodeShouldEqualHTMLCode() {
+        // get API code
+        ResponseEntity<String> apiRes = restTemplate
+                .getForEntity("/api/code", String.class);
+
+        DocumentContext documentContext = JsonPath.parse(apiRes.getBody());
+        String apiCode = documentContext.read("$.code");
+
+        // get HTML code
+        String htmlRes = restTemplate.getForObject("/code", String.class);
+        Document doc = Jsoup.parse(htmlRes);
+
+        Element codeSnippet = doc.getElementById("code_snippet");
+        String htmlCode = codeSnippet.text();
+
+        assertEquals(apiCode, htmlCode);
+    }
+
+    private boolean dateCheck(String date1, String date2) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        long time1 = LocalDateTime.parse(date1, formatter).toEpochSecond(ZoneOffset.UTC);
+        long time2 = LocalDateTime.parse(date2, formatter).toEpochSecond(ZoneOffset.UTC);
+        if(time2 - 1 <= time1 && time1 <= time2 + 1) {
+            return true;
+        }
+        if(time1 - 1 <= time2 && time2 <= time1 + 1) {
+            return true;
+        }
+        return false;
     }
 }
