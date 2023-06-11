c55431867c9e66b603c55c89beb65b7f1e622f21
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-06-11 19:50:23 +0200

    Store latestCode as a public static variable (for now)

--- code-sharing-platform/src/main/java/platform/CodeSharingPlatformApplication.java
+++ code-sharing-platform/src/main/java/platform/CodeSharingPlatformApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CodeSharingPlatformApplication {
 
+	public static Code latestCode = new Code("int a = 0;");
+
 	public static void main(String[] args) {
 		SpringApplication.run(CodeSharingPlatformApplication.class, args);
 	}
--- code-sharing-platform/src/main/java/platform/WebController.java
+++ code-sharing-platform/src/main/java/platform/WebController.java
@@ -1,13 +1,15 @@
 package platform;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
 
     @GetMapping("/code")
-    public String index() {
+    public String getLatestCode(Model model) {
+        model.addAttribute("code", CodeSharingPlatformApplication.latestCode);
         return "index.html";
     }
 }
