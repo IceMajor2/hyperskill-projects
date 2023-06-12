846a62ca20edb66e67a645bd09f2130c8721eaa6
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-06-12 15:41:03 +0200

    Create '/code/{N}' endpoint

--- code-sharing-platform/src/main/java/platform/controllers/WebController.java
+++ code-sharing-platform/src/main/java/platform/controllers/WebController.java
@@ -3,20 +3,30 @@ package platform.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import platform.CodeSharingPlatformApplication;
+import platform.models.Code;
+import platform.repositories.CodeRepository;
 
 @Controller
 public class WebController {
 
-    @GetMapping("/code")
-    public String getLatestCode(Model model) {
-        //model.addAttribute("date", CodeSharingPlatformApplication.latestCode.getDate());
-        //model.addAttribute("code", CodeSharingPlatformApplication.latestCode.getCode());
-        return "index";
+    private CodeRepository codeRepository;
+
+    public WebController(CodeRepository codeRepository) {
+        this.codeRepository = codeRepository;
     }
 
     @GetMapping("/code/new")
     public String getCreateCode() {
         return "new_code";
+    }
+
+    @GetMapping("/code/{N}")
+    public String getNCode(@PathVariable("N") Long id, Model model) {
+        Code code = codeRepository.get(id);
+        model.addAttribute("code", code.getCode());
+        model.addAttribute("date", code.getDate());
+        return "index";
     }
 }
