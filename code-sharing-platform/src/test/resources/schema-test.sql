b1bb474b39e712bcbd899d130ce99817697e8251
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-06-14 16:32:05 +0200

    Add column

--- code-sharing-platform/src/test/resources/schema-test.sql
+++ code-sharing-platform/src/test/resources/schema-test.sql
@@ -5,6 +5,7 @@ CREATE TABLE IF NOT EXISTS codes (
     code TEXT NOT NULL,
     date TIMESTAMP NOT NULL,
     time BIGINT NOT NULL,
+    initial_time BIGINT NOT NULL,
     views BIGINT NOT NULL,
     restricted BOOLEAN DEFAULT false NOT NULL,
     to_be_time_restricted BOOLEAN NOT NULL,
