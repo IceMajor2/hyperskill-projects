2a44bea4753a2f5973ecd0b54a108ad25d8af09d
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-06-15 12:04:20 +0200

    Add time and views restriction

--- code-sharing-platform/src/main/resources/static/js/script.js
+++ code-sharing-platform/src/main/resources/static/js/script.js
@@ -1,10 +1,12 @@
 function send() {
     let object = {
-        "code": document.getElementById("code_snippet").value
+        "code": document.getElementById("code_snippet").value,
+        "views": document.getElementById("views_restriction").value,
+        "time": document.getElementById("time_restriction").value
     };
-    
+
     let json = JSON.stringify(object);
-    
+
     let xhr = new XMLHttpRequest();
     xhr.open("POST", '/api/code/new', false)
     xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
