947b158b3c5b214ceef9044adcacaf10fd6a7d6e
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-10-13 11:58:28 +0200

    Remove Gitman project

--- Gitman/main.py
+++
@@ -1,9 +0,0 @@
-def main() -> None:
-    name = input("What's your name? ")
-    print(f"Hello, {name}!")
-    print("Nice to meet you!")
-
-
-if __name__ == "__main__":
-    print("Hello, world!")
-    main()
