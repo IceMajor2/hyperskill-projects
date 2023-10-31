2608e99ba34ed33be748f1cf771259c57de1958d
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-10-31 08:02:35 +0100

    Unwrap from ResponseEntity the returned objects

--- account-service/src/main/java/account/controller/BusinessController.java
+++ account-service/src/main/java/account/controller/BusinessController.java
@@ -4,7 +4,6 @@ import account.dto.AuthPaymentDTO;
 import account.dto.PaymentDTO;
 import account.service.BusinessService;
 import jakarta.validation.Valid;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -26,29 +25,27 @@ public class BusinessController {
 
     @GetMapping(value = {"/api/empl/payment", "/api/empl/payment/"}, params = "period")
     @PreAuthorize("hasAuthority('ROLE_USER') or hasAuthority('ROLE_ACCOUNTANT')")
-    public ResponseEntity getPayroll(@AuthenticationPrincipal UserDetails userDetails, @RequestParam String period) {
-        AuthPaymentDTO authPaymentDTO = businessService.getPayrolls(userDetails, period);
-        return ResponseEntity.ok(authPaymentDTO);
+    public AuthPaymentDTO getPayroll(@AuthenticationPrincipal UserDetails userDetails, @RequestParam String period) {
+        return businessService.getPayrolls(userDetails, period);
     }
 
     @GetMapping(value = {"/api/empl/payment", "/api/empl/payment/"})
     @PreAuthorize("hasAuthority('ROLE_USER') or hasAuthority('ROLE_ACCOUNTANT')")
-    public ResponseEntity getPayroll(@AuthenticationPrincipal UserDetails userDetails) {
-        List<AuthPaymentDTO> payments = businessService.getPayrolls(userDetails);
-        return ResponseEntity.ok(payments);
+    public List<AuthPaymentDTO> getPayroll(@AuthenticationPrincipal UserDetails userDetails) {
+        return businessService.getPayrolls(userDetails);
     }
 
     @PostMapping(value = {"/api/acct/payments", "/api/acct/payments/"})
     @PreAuthorize("hasAuthority('ROLE_ACCOUNTANT')")
-    public ResponseEntity uploadPayroll(@RequestBody List<@Valid PaymentDTO> paymentDTOS) {
+    public Map<String, String> uploadPayroll(@RequestBody List<@Valid PaymentDTO> paymentDTOS) {
         businessService.uploadPayrolls(paymentDTOS);
-        return ResponseEntity.ok(Map.of("status", "Added successfully!"));
+        return Map.of("status", "Added successfully!");
     }
 
     @PutMapping(value = {"/api/acct/payments", "/api/acct/payments/"})
     @PreAuthorize("hasAuthority('ROLE_ACCOUNTANT')")
-    public ResponseEntity updatePayment(@RequestBody @Valid PaymentDTO paymentDTO) {
+    public Map<String, String> updatePayment(@RequestBody @Valid PaymentDTO paymentDTO) {
         businessService.updatePayment(paymentDTO);
-        return ResponseEntity.ok(Map.of("status", "Updated successfully!"));
+        return Map.of("status", "Updated successfully!");
     }
 }
--- account-service/src/main/java/account/controller/SecurityController.java
+++ account-service/src/main/java/account/controller/SecurityController.java
@@ -1,24 +1,25 @@
 package account.controller;
 
+import account.model.SecurityLog;
 import account.service.SecurityLogService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/security")
+@RequiredArgsConstructor
 public class SecurityController {
 
-    @Autowired
-    private SecurityLogService securityLogService;
+    private final SecurityLogService securityLogService;
 
     @GetMapping(value = {"/events", "/events/"})
     @PreAuthorize("hasAuthority('ROLE_AUDITOR')")
-    public ResponseEntity getLogs() {
-        var logs = securityLogService.getLogs();
-        return ResponseEntity.ok(logs);
+    public List<SecurityLog> getLogs() {
+        return securityLogService.getLogs();
     }
 }
